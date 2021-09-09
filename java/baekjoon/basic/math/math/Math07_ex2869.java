9934fb6f75b79b539159af7bfaeedf361e71a2d2
YongLee <70495425+nogy21@users.noreply.github.com>
2021-09-10 00:03:30 +0900

    math04-09

---
+++ java/baekjoon/basic/math/math/Math07_ex2869.java
@@ -0,0 +1,24 @@
+package basic.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Math07_ex2869 {
+	public static void main(String[] args) throws IOException {
+		//Scanner보다 효율적인 처리를 위해 BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//한 줄에 공백을 두고 입력받기에 StringTokenizer 사용
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int A = Integer.parseInt(st.nextToken());//올라감
+		int B = Integer.parseInt(st.nextToken());//내려감
+		int V = Integer.parseInt(st.nextToken());//나무막대길이
+		int D = (V-B)/(A-B);// 도착일
+		if((V-B)%(A-B)!=0)// A*D - B*(D-1) >= V
+			D = (V-B)/(A-B)+1;
+		System.out.println(D);
+	}
+}
