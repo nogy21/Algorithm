d415ab2c653bc20b79f50f43f6189c9d368f453f
YongLee <70495425+nogy21@users.noreply.github.com>
2021-09-23 23:54:01 +0900

    BOJ(4948-베르트랑 공준, 9020-골드바흐의 추측)
    https://www.acmicpc.net/problem/4948
    https://www.acmicpc.net/problem/9020

---
+++ java/baekjoon/basic/math2/Math06_ex4948.java
@@ -0,0 +1,32 @@
+package basic.math2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Math06_ex4948 {
+	public static boolean check[] = new boolean[246913];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			int n = Integer.parseInt(br.readLine());
+			if (n == 0)	break;
+			System.out.println(cntPrimeNum(n));
+		}
+	}
+
+	public static int cntPrimeNum(int n) {// 범위 잘못 생각한듯 답은 맞게 나오는데...
+		check[0] = check[1] = true;
+		for (int i = 2; i <= Math.sqrt(check.length); i++) {
+			if (check[i]) continue;
+			for (int j = i * i; j < check.length; j += i) {
+				check[j] = true;
+			}
+		}
+		int cnt = 0;
+		for (int i = n+1; i <= 2 * n; i++) {
+			if (!check[i]) cnt++;
+		}
+		return cnt;
+	}
+}
