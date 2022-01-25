e6a69d630cc23d49ff63d0b360db4c95ff4b6991
Yong Lee <nogy21@gmail.com>
2022-01-25 15:48:26 +0900

    [이취코테] 그리디 문제 3 - 모험가 길드 (모범 예시)

--- ndb-python-for-coding-test(java)/src/greedy/ex4.java
+++ ndb-python-for-coding-test(java)/src/greedy/ex4.java
@@ -1,9 +1,9 @@
 package greedy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ex4 {
 
@@ -11,25 +11,29 @@ public class ex4 {
  * 모험가 길드
  *  공포도가 X인 모험가는 반드시 X명 이상으로 구성한 모험가 그룹에 참여해야 한다.
  *  여행을 떠날 수 있는 그룹 수의 최댓값을 구하는 프로그램을 작성하라.
- *  
+ *
  */
+    public static int n;
+    public static ArrayList<Integer> arrayList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         // 모험가의 수
-        int n = Integer.parseInt(br.readLine());
+        n = sc.nextInt();
         // 각 모험가의 공포도
-        String[] list = br.readLine().split(" ");
-
+        // String[] list = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arrayList.add(sc.nextInt());
+        }
         // 오름차순 정렬
-        Arrays.sort(list);
+        Collections.sort(arrayList);
 
         int result = 0; // 총 그룹 수
         int cnt = 0; // 그룹에 속해진 모험가 수
 
         for(int i = 0; i < n; i++) { // 각 공포도를 확인
             cnt ++; // 그룹에 포함
-            if(cnt >= Integer.parseInt(list[i])) { // 그룹에 속한 수가 공포도 이상
+            if(cnt >= arrayList.get(i)) { // 그룹에 속한 수가 공포도 이상
                 result ++;
                 cnt = 0; // 초기화
             }
