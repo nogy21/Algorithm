f2c9205cebcfcfdcbf4f502f3aedb621c9eb2261
Yong Lee <nogy21@gmail.com>
2022-02-09 00:05:39 +0900

    BOJ(11651-좌표 정렬하기 2)
    https://www.acmicpc.net/problem/11651

---
+++ BOJ/src/basic/sort/Ex11651.java
@@ -0,0 +1,41 @@
+package basic.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * 좌표 정렬하기 2(https://www.acmicpc.net/problem/11651)
+ */
+public class Ex11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] x, int[] y) {
+                if (x[1] == y[1]) {
+                    return x[0] - y[0];
+                }
+                return x[1] - y[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+
+}
