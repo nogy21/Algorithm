c216c2881e68856eac300edd0e9d579ddad3f7ed
Yong Lee <nogy21@gmail.com>
2022-02-10 11:51:44 +0900

    디렉토리 수정

--- ndb-python-for-coding-test(java)/src/dfsnbfs/ex1.java
+++ ndb-python-for-coding-test(java)/src/dfsnbfs/ex1.java
@@ -1,4 +1,4 @@
-package dfs;
+package dfsnbfs;
 
 import java.util.ArrayList;
 
@@ -9,19 +9,6 @@ public class ex1 {
 
     public static boolean[] visited = new boolean[9];
     public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-
-    // DFS 함수 정의
-    public static void dfs(int x) {
-        // 현재 노드를 방문 처리
-        visited[x] = true;
-        System.out.print(x + " ");
-        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for (int i = 0; i < graph.get(x).size(); i++) {
-            int y = graph.get(x).get(i);
-            if (!visited[y]) dfs(y);
-        }
-    }
-
     public static void main(String[] args) {
         // 그래프 초기화
         for (int i = 0; i < 9; i++) {
@@ -63,6 +50,18 @@ public class ex1 {
         graph.get(8).add(7);
 
         dfs(1);
+    }
+
+    // DFS 함수 정의
+    public static void dfs(int x) {
+        // 현재 노드를 방문 처리
+        visited[x] = true;
+        System.out.print(x + " ");
+        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+        for (int i = 0; i < graph.get(x).size(); i++) {
+            int y = graph.get(x).get(i);
+            if (!visited[y]) dfs(y);
+        }
     }
 
 }
