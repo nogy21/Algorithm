711b544328a2b78429fd295d99f9e573798dccd7
Yong Lee <nogy21@gmail.com>
2022-02-20 23:59:37 +0900

    2667

---
+++ BOJ/src/basic/dfsnbfs/Ex2667.java
@@ -0,0 +1,99 @@
+package basic.dfsnbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 단지번호붙이기(https://www.acmicpc.net/problem/2667)
+ */
+public class Ex2667 {
+    static int n, apartNum = 0;
+    private static int[] aparts = new int[25 * 25];
+    static StringBuilder sb = new StringBuilder();
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    apartNum++;
+                    bfs(i, j);
+                }
+            }
+        }
+
+        Arrays.sort(aparts);
+        System.out.println(apartNum);
+
+        for (int i = 0; i < aparts.length; i++) {
+            if (aparts[i] == 0) {
+            } else {
+                System.out.println(aparts[i]);
+            }
+        }
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y));
+        visited[x][y] = true;
+        aparts[apartNum]++;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                    continue;
+                }
+
+                if(map[nx][ny] == 1 && !visited[nx][ny]){
+                    q.offer(new Node(nx, ny));
+                    visited[nx][ny] = true;
+                    aparts[apartNum]++;
+                }
+            }
+        }
+    }
+
+    private static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return this.x;
+        }
+
+        public int getY() {
+            return this.y;
+        }
+    }
+}
