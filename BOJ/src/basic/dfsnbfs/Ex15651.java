4aee3b099322953b8f0c32a7a22b7ed4753c99b5
Yong Lee <nogy21@gmail.com>
2022-02-24 00:07:42 +0900

    BOJ(15651-N과 M (3)) - 백트래킹
    https://www.acmicpc.net/problem/15651

---
+++ BOJ/src/basic/dfsnbfs/Ex15651.java
@@ -0,0 +1,41 @@
+package basic.dfsnbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * N과 M(3) (https://www.acmicpc.net/problem/15651)
+ */
+public class Ex15651 {
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
