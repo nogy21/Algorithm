9e635feda21d1c40bf44483be5e7c8d0f377f86c
Yong Lee <nogy21@gmail.com>
2022-03-10 19:12:25 +0900

    BOJ(11659-구간 합 구하기 4) segment tree
    https://www.acmicpc.net/problem/11659

---
+++ BOJ/src/basic/segment/Ex11659.java
@@ -0,0 +1,39 @@
+package basic.segment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 구간 합 구하기 4(https://www.acmicpc.net/problem/11659)
+ */
+public class Ex11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        int[] prefixSum = new int[n + 1];
+
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum += arr[i];
+            prefixSum[i] = sum;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int start, end, result;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+            result = prefixSum[end] - prefixSum[start - 1];
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
