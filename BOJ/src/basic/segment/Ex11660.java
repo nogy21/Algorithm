39b4c72b6255795f08d95d774cf39db191eba90c
Yong Lee <nogy21@gmail.com>
2022-03-10 20:44:46 +0900

    코드 수정
    불필요한 코드 제거

--- BOJ/src/basic/segment/Ex11660.java
+++ BOJ/src/basic/segment/Ex11660.java
@@ -1,8 +1,6 @@
 package basic.segment;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 /**
@@ -16,19 +14,18 @@ public class Ex11660 {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n + 1][n + 1];
         int[][] prefixSum = new int[n + 1][n + 1];
-        int sum = 0;
 
+        int tmp;
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + arr[i][j];
+                tmp = Integer.parseInt(st.nextToken());
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + tmp;
             }
         }
 
-        int sX, sY, eX, eY, tmp;
+        int sX, sY, eX, eY;
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
