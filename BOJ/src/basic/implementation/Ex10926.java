39d71dd5a4731d34062b78e47854a903e702f4f8
Yong Lee <nogy21@gmail.com>
2022-03-13 23:35:11 +0900

    BOJ(10926-??!) 구현
    https://www.acmicpc.net/problem/10926

--- BOJ/src/basic/implementation/Ex10926.java
+++ BOJ/src/basic/implementation/Ex10926.java
@@ -1,2 +1,16 @@
-package basic.implementation;public class Ex10926 {
+package basic.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * ??! (https://www.acmicpc.net/problem/10926)
+ */
+public class Ex10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input + "??!");
+    }
 }
