0ca0ffed170ff9d5d0d13425d796cc63f4f2b3b5
Yong Lee <nogy21@gmail.com>
2022-03-20 00:21:36 +0900

    주석 추가

--- BOJ/src/basic/stack/Ex17298.java
+++ BOJ/src/basic/stack/Ex17298.java
@@ -3,6 +3,15 @@ package basic.stack;
 import java.io.*;
 import java.util.*;
 
+/**
+ * 오큰수 (https://www.acmicpc.net/problem/17298)
+ * 배열에 수열 입력
+ * 반복: 수열의 첫 번째 요소부터 확인
+ *     조건,반복: 이전 값 중 현재 값보다 작은 값 존재
+ *         배열(스택에서 꺼낸 인덱스)에 현재 값 저장
+ *     스택에 현재 인덱스 저장
+ * 남은 값 -1 처리
+ */
 public class Ex17298 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
