9a8dd03d61f7de60431a9aa4b349515f6e8e2b81
Yong Lee <nogy21@gmail.com>
2022-04-03 23:19:04 +0900

    BOJ(7576-토마토) BFS - 코드 개선(반복문 제거, 카운트하는 변수 사용)
    https://www.acmicpc.net/problem/7576

--- BOJ/src/basic/dfsnbfs/Ex7576.java
+++ BOJ/src/basic/dfsnbfs/Ex7576.java
@@ -6,16 +6,16 @@ import java.util.*;
 /**
  * 토마토 (https://www.acmicpc.net/problem/7576)
  * 아이디어 (bfs)
- * - 익은 토마토의 위치에서 모두 bfs 진행
- * - 인접한 토마토가 익을 때(방문) 카운트(익은 토마토 개수와 bfs 탐색 회수 별도로 저장)
+ * - 익은 토마토의 위치에서 모두 bfs 진행 => 익은 정도를 객체에 담아두고 Queue 에 넣어둠
+ * - 인접한 토마토가 익을 때(방문) 카운트(익은 토마토 개수와 bfs 탐색 회수 별도로 저장) => 별도 저장 필요 X, 최대 값만 갱신
  * - bfs 종료 시, 익은 토마토 개수와 처음 입력받은 익지 않은 토마토 개수 비교
- * <p>
+ *
  * 시간복잡도
  * : 2 <= M, N <= 1000
  * O(MN) == 1,000,000
  */
 public class Ex7576 {
-    static int M, N, days = 0;
+    static int M, N, days = 0, unripeTomatoes = 0;
     static int[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
@@ -24,7 +24,7 @@ public class Ex7576 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 입력력
+        // 입력
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
@@ -39,6 +39,8 @@ public class Ex7576 {
 
                 if (input == 1) {
                     tomatoes.add(new Tomato(i, j, 1));
+                } else if (input == 0) {
+                    unripeTomatoes++;
                 }
             }
         }
@@ -46,16 +48,17 @@ public class Ex7576 {
         bfs();
 
         // 안 익은 토마토 확인
-        loop: for (int[] rows : map) {
+        /*loop: for (int[] rows : map) {
             for (int val : rows) {
                 if (val == 0) {
                     days = -1;
                     break loop;
                 }
             }
-        }
+        }*/
 
-        days = days == -1 ? -1 : days - 1;
+//        days = days == -1 ? -1 : days - 1;
+        days = unripeTomatoes > 0 ? -1 : days - 1;
         System.out.println(days);
     }
 
@@ -75,6 +78,7 @@ public class Ex7576 {
 
                 if (map[nx][ny] == 0) {
                     map[nx][ny] = 1;
+                    unripeTomatoes--;
                     tomatoes.offer(new Tomato(nx, ny, tomato.time + 1));
                 }
             }
