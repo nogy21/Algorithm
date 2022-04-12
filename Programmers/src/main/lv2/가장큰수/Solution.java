aba22ca2588cf37ea4c306c0882bb8159af45092
Yong Lee <nogy21@gmail.com>
2022-04-12 16:52:30 +0900

    Programmers lv2 가장큰수 - 코드 수정(Arrays.sort() -> Collections.sort())

--- Programmers/src/main/lv2/가장큰수/Solution.java
+++ Programmers/src/main/lv2/가장큰수/Solution.java
@@ -2,15 +2,14 @@ package main.lv2.가장큰수;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collections;
 
 /**
  * 고득점 Kit 정렬
  * - 가장 큰 수 (https://programmers.co.kr/learn/courses/30/lessons/42746)
  *
  * 아이디어
- * :
+ * : 배열 요소 문자열 변환 후 arr[0] + arr[1] 과 arr[1] + arr[0] 비교 후 정렬
  */
 
 public class Solution {
@@ -20,23 +19,23 @@ public class Solution {
             return "0";
         }
 
-        String[] arr = makeStringArr(numbers);
+        ArrayList<String> arr = makeStringArr(numbers);
 
         // 두 문자 a+b 와 b+a 비교 -> b+a가 더 크면 b를 앞으로
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Collections.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
         return makeLargestNumber(arr);
     }
 
-    private String[] makeStringArr(int[] numbers) {
-        String[] retArr = new String[numbers.length];
+    private ArrayList<String> makeStringArr(int[] numbers) {
+        ArrayList<String> retArr = new ArrayList<>();
         for (int i = 0; i < numbers.length; i++) {
-            retArr[i] = String.valueOf(numbers[i]);
+            retArr.add(String.valueOf(numbers[i]));
         }
         return retArr;
     }
 
-    private String makeLargestNumber(String[] arr) {
+    private String makeLargestNumber(ArrayList<String> arr) {
         String ret = "";
         for (String str : arr) {
             ret += str;
