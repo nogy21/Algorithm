f40a4d600d947556ee40c1319b88bec5e532e9c5
Yong Lee <nogy21@gmail.com>
2022-05-09 22:18:59 +0900

    BOJ(10816 숫자 카드2) 이분 탐색

---
+++ BOJ/src/basic/binarysearch/Ex10816.java
@@ -0,0 +1,36 @@
+package basic.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+/**
+ * 숫자 카드2 (https://www.acmicpc.net/problem/10816)
+ * : 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다.
+ *  정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램
+ */
+public class Ex10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(target, 0)).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
