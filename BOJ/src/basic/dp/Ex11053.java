c5d1742dba8dea8ed8e9a80f6d4bc69c63cb8233
Yong Lee <nogy21@gmail.com>
2022-06-29 23:44:55 +0900

    BOJ(11053 가장 긴 증가하는 부분 수열) DP

---
+++ BOJ/src/basic/dp/Ex11053.java
@@ -0,0 +1,48 @@
+package basic.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 가장 긴 증가하는 부분 수열 (https://www.acmicpc.net/problem/11053)
+ * : 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+ *  예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
+ *
+ * 입력
+ * : 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+ *  둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+ *
+ * 출력
+ * : 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
+ *
+ * 아이디어
+ * : DP, bottom-up, O(n^2)
+ */
+public class Ex11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
